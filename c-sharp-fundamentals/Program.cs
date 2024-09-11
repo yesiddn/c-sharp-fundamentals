4e8a3152740da4f84ea76ad369dce4e720f342f8
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-09-11 11:17:14 -0500

    Add use of variables

--- c-sharp-fundamentals/Program.cs
+++ c-sharp-fundamentals/Program.cs
@@ -1,9 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
 // Programa que calcula el área de un rectangulo
-int ladoA;
-int ladoB;
-int area;
+double ladoA;
+double ladoB;
+double area;
 
 Console.WriteLine("Calcula el área de un rectángulo");
+Console.WriteLine("Ingresa el valor del lado A");
+ladoA = Convert.ToDouble(Console.ReadLine()); // lee un valor desde la consola
+
+Console.WriteLine("Ingresa el valor del lado B");
+ladoB = Convert.ToDouble(Console.ReadLine()); // ToInt32 convierte el valor a entero
+area = ladoA * ladoB;
+
+Console.WriteLine("El área del rectángulo es: " + area);
