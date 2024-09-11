acf3d6a7ab476165e2e1084fc0fdf50dd13012bc
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-09-11 15:16:18 -0500

    Add arithmetic operators

---
+++ arithmetic-operators/arithmetic-operators/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int a = 18;
+int b = 6;
+int x;
+
+a++; // operador de incremento -> a = a + 1 -> suma 1 a la variable a
+a--; // operador de decremento -> a = a - 1 -> resta 1 a la variable a
+
+x = a + b;
+x = a - b;
+x = a * b;
+x = a / b;
+x = a % b; // operador de módulo -> resto de la división de a entre b
+x = a * b / 2; // primero se multiplica a por b y luego se divide el resultado entre 2 porque se ejecuta de izquierda a derecha
+x = (a * b) / 2; // para luego no crear errores, es mejor poner paréntesis para ordenar las operaciones como nosotros queramos
+
