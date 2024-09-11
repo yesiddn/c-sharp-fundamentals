06ebe0865e5f0d5518ba85c720f15b3f5e3dc8a5
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-09-11 10:21:43 -0500

    Add project files.

---
+++ c-sharp-fundamentals/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Programa que calcula el área de un rectangulo
+int ladoA;
+int ladoB;
+int area;
+
+Console.WriteLine("Calcula el área de un rectángulo");
