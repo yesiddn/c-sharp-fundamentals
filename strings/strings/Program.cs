a36001768b80630add9f89775a6ab332568c3ff8
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-09-12 09:39:52 -0500

    Add strings

---
+++ strings/strings/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Duvan Yesid ID");
+
+int height = 170;
+int age = 20;
+string name = "Duvan Yesid";
+string info = "Nacio en Colombia, es estudiante de astronomia y tiene 20 años";
+var hobby = "Voleibol";
+
+string idCard = $"Nombre: {name}, \nEdad: {age}, \nAltura: {height} cm, \nHobby: {hobby}, \nInfo: {info}";
+
+Console.WriteLine(idCard);
